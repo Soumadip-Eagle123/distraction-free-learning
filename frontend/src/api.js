434a6cecd74722bebc27ae2262c4a6741d5df973
Soumadip Sen <soumadipsen1@gmail.com>
2026-03-20 22:00:02 +0530

    fix: add https to API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "distraction-free-learning-production.up.railway.app";
+const API = "https://distraction-free-learning-production.up.railway.app";
 
 export const getCourses = () => axios.get(`${API}/courses`);
 export const getProgress = (id) => axios.get(`${API}/progress/${id}`);
