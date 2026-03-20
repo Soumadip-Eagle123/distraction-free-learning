bf1d2e737d1894416129da31d259317cf1a55fa6
Soumadip Sen <soumadipsen1@gmail.com>
2026-03-20 21:51:16 +0530

    fix: update API URL to Railway backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5000";
+const API = "distraction-free-learning-production.up.railway.app";
 
 export const getCourses = () => axios.get(`${API}/courses`);
 export const getProgress = (id) => axios.get(`${API}/progress/${id}`);
